b84ec4fbec5b24ef80ad284b64ca2a56d47b6612
KyleJPNV <jpnv@iup.edu>
2018-09-16 20:02:52 -0400

    api folder structure

---
+++ src/main/java/com/cosc465/ZEKsports/ZeKsportsApplication.java
@@ -0,0 +1,12 @@
+package com.cosc465.ZEKsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeKsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeKsportsApplication.class, args);
+	}
+}
